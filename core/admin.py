6230f1c48bc305d2b538542121d5344c75c56f99
Simran23772 <simran.220180107030@gmail.com>
2025-12-02 11:40:35 +0530

    hattt commit

--- core/admin.py
+++ core/admin.py
@@ -8,4 +8,17 @@ class SeatAdmin(admin.ModelAdmin):
 
 admin.site.register(LandingFormData)
 admin.site.register(PaymentScreenshot)
-admin.site.register(SelectedSeat)
+
+@admin.register(SelectedSeat)
+class SelectedSeatAdmin(admin.ModelAdmin):
+	list_display = ('get_user_name', 'get_seat_labels', 'selected_at')
+
+	def get_user_name(self, obj):
+		return obj.user.name
+	get_user_name.short_description = 'User Name'
+
+	def get_seat_labels(self, obj):
+		# Get all seats selected by this user
+		seats = SelectedSeat.objects.filter(user=obj.user).values_list('seat__seat_number', flat=True)
+		return ', '.join(seats)
+	get_seat_labels.short_description = 'Seats'
