9986ae913c59d2952d83fd52dbac979c53586bdc
Simran23772 <simran.220180107030@gmail.com>
2025-12-02 11:07:55 +0530

    Change commit

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,11 @@
+
 from django.contrib import admin
 from .models import LandingFormData, PaymentScreenshot, Seat, SelectedSeat
 
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+	list_display = ('seat_number', 'is_booked')
+
 admin.site.register(LandingFormData)
 admin.site.register(PaymentScreenshot)
-admin.site.register(Seat)
 admin.site.register(SelectedSeat)
