8a25c6d29e1d114455a4f9f57eb6c984232854e8
Gagan Rathod <gaganclickworker347@gmail.com>
2025-12-13 11:45:12 +0530

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,7 @@ from core.models import SelectedSeat, Seat
 class LandingForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
     phone = forms.CharField(max_length=15, required=True, label='Phone Number')
+    email = forms.EmailField(required=False, label='Email (optional)')
     dob = forms.DateField(required=True, label='Date of Birth', widget=forms.DateInput(attrs={'type': 'date'}))
 
 class PaymentScreenshotForm(forms.Form):
